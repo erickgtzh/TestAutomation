6b264f50157b19cd8b77d441651e97449ba3e0c7
ErickGutierrez <erickgtz@gmail.com>
2020-04-18 00:03:11 -0500

    test using pytest

---
+++ test/manual_test.py
@@ -0,0 +1,42 @@
+import datetime
+import pytz
+from models.utils import read_serial, wait
+from suite import call_adb, call_uia, manage_wifi_status
+
+script_manager = [manage_wifi_status]
+
+
+def suite_execution():
+    for suite in script_manager:
+        try:
+            serial = suite.read_serial()
+        except Exception as ex:
+            print('Error: ' + ex)
+            pass
+
+        start_ts_pst = suite.get_time()
+        print 'test start:  {}'.format(start_ts_pst)
+        print 'serial    :  {}'.format(serial)
+
+        try:
+            print suite.suite_info()
+            suite.suite_methods()
+            wait()
+
+        except Exception as ex:
+            print(ex)
+            pass
+        finally:
+            stop_ts_pst = suite.get_time()
+
+            print "-------------RESULTS-------------"  # results of out scripts
+            print 'test start: {0}\ntest end: {1}'.format(start_ts_pst, stop_ts_pst)  # our script end time parsed
+
+
+# ---------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    """
+    Where is managed what methods are going to run
+    """
+    suite_execution()
